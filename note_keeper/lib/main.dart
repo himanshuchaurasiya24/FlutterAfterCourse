bc96aa218c7fbf1a299e3eeb5206f93b06ef83f1
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2023-11-14 13:03:29 +0530

    note_keeper_app_database_opened

--- note_keeper/lib/main.dart
+++ note_keeper/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper/database/database.dart';
 import 'package:note_keeper/screen/note_list_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,35 +13,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-          primarySwatch: Colors.deepOrange,
-          textTheme: const TextTheme(
-            headline5: TextStyle(
-                fontFamily: 'Sans',
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 24),
-            bodyText2: TextStyle(
-                fontFamily: 'Sans',
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 20),
-            bodyText1: TextStyle(
-                fontFamily: 'Sans',
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-                fontSize: 18),
-            subtitle2: TextStyle(
-                fontFamily: 'Sans',
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-                fontSize: 14),
-          )),
-      home: const NoteListPage(),
+    return Provider(
+      create: (context) {
+        return AppDatabase();
+      },
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+            primarySwatch: Colors.deepOrange,
+            textTheme: const TextTheme(
+              headline5: TextStyle(
+                  fontFamily: 'Sans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 24),
+              bodyText2: TextStyle(
+                  fontFamily: 'Sans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 20),
+              bodyText1: TextStyle(
+                  fontFamily: 'Sans',
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                  fontSize: 18),
+              subtitle2: TextStyle(
+                  fontFamily: 'Sans',
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                  fontSize: 14),
+            )),
+        home: const NoteListPage(),
+      ),
     );
   }
 }
